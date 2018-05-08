e215e428e66412d801dc674110aba055fb088da2
keira25 <Keira1425@gmail.com>
2018-05-09 06:26:32 +0800

    user registration and validation

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 // Making object available outside file
 module.exports = {
   mongoURI:
-    "mongodb://teampsbox:TEAMPSBOX1@ds115360.mlab.com:15360/teampsboxsite"
+    "mongodb://teampsbox:TEAMPSBOX1@ds115360.mlab.com:15360/teampsboxsite",
+  secretOrKey: "secret"
 };
